9317fcc709814d9930d76d0beb35d51cc2e46436
Panhe-C <983353213@qq.com>
2019-07-27 17:32:58 +0800

    第一次尝试提交

---
+++ src/test/test1.java
@@ -0,0 +1,9 @@
+package test;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        int num = 32;
+        System.out.println(num >> 3);
+    }
+}
---
+++ src/test/酒杯.java
@@ -0,0 +1,19 @@
+package test;
+
+public class 酒杯 {
+
+    public int count(int i,int j){
+        int result = 0;
+
+        if(i == j || j ==1){
+            result = i*(i+1)/2;
+        }else{
+
+        }
+
+
+
+
+        return result;
+    }
+}

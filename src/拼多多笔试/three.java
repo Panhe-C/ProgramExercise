04a792dc2cfc31cebd07cc7ee6b9fc15052d6f1c
Panhe-C <983353213@qq.com>
2019-07-28 14:16:13 +0800

    pinduoduo

--- src/拼多多笔试/three.java
+++ src/拼多多笔试/three.java
@@ -1,4 +1,15 @@
 package 拼多多笔试;
 
+import java.util.Scanner;
+
 public class three {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()){
+            int N = scanner.nextInt();
+            System.out.println();
+        }
+    }
 }
